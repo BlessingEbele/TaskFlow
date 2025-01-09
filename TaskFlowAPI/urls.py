d4a05341019e05d6e8bee4170d30ba042ae80daf
blessingebele <blessmart.com@gmail.com>
2025-01-09 21:08:16 +0100

    frontend static

--- TaskFlowAPI/urls.py
+++ TaskFlowAPI/urls.py
@@ -19,21 +19,15 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_viewis
 from rest_framework.authtoken.views import obtain_auth_token 
 
-# from .views import TaskListCreateAPIView, TaskDetailAPIView, UserListAPIView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    #path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/', obtain_auth_token),  # Token Authentication Endpoint
-    # path('tasks/', TaskListCreateAPIView.as_view(), name='task-list-create'),
-    # path('tasks/<int:pk>/', TaskDetailAPIView.as_view(), name='task-detail'),
-    # path('users/', UserListAPIView.as_view(), name='user-list'),  # Optional for superuser testing
+
 ]
 
-# , namespace='rest_framework
 #after frontend configuration
 from django.contrib.auth.views import LoginView, LogoutView
 
